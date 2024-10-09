d120250116141595221c895bf88c6b802fb493ec
jason <jason_zong@hotmail.com>
2024-10-09 18:54:54 +0800

    fix: 查询缓存数据不存在，查询数据库，并更新缓存值

--- src/main/java/cn/yishotech/starter/redisson/annotation/RedissonCache.java
+++ src/main/java/cn/yishotech/starter/redisson/annotation/RedissonCache.java
@@ -8,6 +8,7 @@ package cn.yishotech.starter.redisson.annotation;
 import cn.yishotech.starter.redisson.model.DataType;
 
 import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>类路径:cn.yishotech.starter.annotation.RedissonCache</p>
@@ -29,6 +30,16 @@ public @interface RedissonCache {
      * 缓存键
      */
     String[] keys() default "";
+
+    /**
+     * 缓存过期时间，单位为分钟，默认为0，表示不过期
+     */
+    long expire() default 0;
+
+    /**
+     * 缓存过期时间单位，默认为分钟
+     */
+    TimeUnit unit() default TimeUnit.MINUTES;
 
     /**
      * 查询数据类型
